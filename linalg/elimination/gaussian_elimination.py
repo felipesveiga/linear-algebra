a89ab25704019c7155d3b9bcc9fbedaced90f5be
felipesveiga <felipesveiga@gmail.com>
2025-01-17 19:43:37 -0300

    Revisar, testar e documentar `GaussianElimination`

--- linalg/elimination/gaussian_elimination.py
+++ linalg/elimination/gaussian_elimination.py
@@ -1,4 +1,5 @@
 import numpy as np
+from warnings import warn
 from typing import Tuple
 
 class GaussianElimination:
@@ -7,6 +8,7 @@ class GaussianElimination:
         self.A = A.astype(np.float32)
         self.P_ = np.identity(A.shape[0]) 
         self.E_ = []
+        self.U_ = None
 
     @staticmethod
     def __check_A(A:np.ndarray)->None:
@@ -46,16 +48,15 @@ class GaussianElimination:
         self.E_ = np.linalg.multi_dot(self.E_) if len(self.E_)>1 else np.array(self.E_[0])
         
     def __eliminate(self, b:np.ndarray=None)->Tuple[np.ndarray] | np.ndarray:
-        print(b)
         M = np.concatenate((self.A, b), axis=1) if b is not None else self.A
         M = np.linalg.multi_dot((self.E_, self.P_, M)) 
         return (M[:, :-1], M[:, -1]) if b is not None else M
 
-    def __solve(self, b:np.ndarray)->np.ndarray:
-        solution, N = np.array([0. for i in range(self.U_.shape[0])]),  -(self.U_.shape[0]+1)
+    def __solve(self, A:np.ndarray, b:np.ndarray)->np.ndarray:
+        solution, N = np.array([0. for i in range(A.shape[0])]),  -(A.shape[0]+1)
         for i in range(-1, N, -1):
-            subtract = np.sum(self.U_[i, i+1:]*solution[i+1:]) if i<-1 else 0
-            solution[i] = (b[i] - subtract)/self.U_[i,i]
+            subtract = np.sum(A[i, i+1:]*solution[i+1:]) if i<-1 else 0
+            solution[i] = (b[i] - subtract)/A[i,i]
         return np.array(solution)
 
     def eliminate(self, b:np.ndarray=None)->Tuple[np.ndarray]:
@@ -66,13 +67,18 @@ class GaussianElimination:
 
     def solve(self, b:np.ndarray=None)->np.ndarray:
         if b is None:
-            raise ValueError('You must specify a `b` vector')
-        b = b.reshape(-1,1) if b.ndim==1 else b
-        if 'U_' not in self.__dict__:
-            self.eliminate(b)
-            return self.__solve(*self.U_)
-        else:
             if isinstance(self.U_, tuple):
                 return self.__solve(*self.U_) 
             else:
-                return self.__solve(self.U_, b)
+                raise ValueError('You must specify a `b` vector')
+        else:
+            b = b.reshape(-1,1) if b.ndim==1 else b
+            if self.U_ is None: 
+                self.eliminate(b)
+                return self.__solve(*self.U_)
+            else:
+                if isinstance(self.U_, np.ndarray):
+                    return self.__solve(self.U_, b) 
+                else:
+                    warn('Elimination was already carried out considedring `b`. You did not have to inform it again on `solve`', UserWarning)
+                    return self.__solve(*self.U_)
