b77d6d4d3b813063a1af2d38174bac23989be103
felipesveiga <felipesveiga@gmail.com>
2025-01-21 06:48:23 -0300

    Continuar documentação da `GaussianElimination`

--- linalg/elimination/gaussian_elimination.py
+++ linalg/elimination/gaussian_elimination.py
@@ -3,6 +3,24 @@ from warnings import warn
 from typing import Tuple
 
 class GaussianElimination:
+    '''
+        Proceeds the Gaussian Elimination of a linear system.
+
+        Parameter
+        ---------
+        `A`: `np.ndarray`
+            The coefficient matrix.
+
+        Attributes
+        ----------
+        `P_`: np.ndarray
+            The row reorder matrix.
+        `E_`: np.ndarray
+            The elimination matrix.
+        `U_`: `np.ndarray` | Tuple[np.ndarray]
+            The eliminated coefficient matrix, or a tuple with such array
+            along with the eliminated target vector.
+    '''
     def __init__(self, A:np.ndarray):
         self.__check_A(A)
         self.A = A.astype(np.float32)
@@ -24,6 +42,15 @@ class GaussianElimination:
         assert np.linalg.det(A) != 0, 'Singular Matrix'  
 
     def __swap_P_rows(self, i:int)->None:
+        '''
+            Exchange a given row by another in case of the first's
+            pivot is 0
+
+            Parameter
+            ---------
+            `i`: int
+                The index of the row that might face exchange.
+        '''
         candidate_subs = np.argwhere(self.A[i+1:, i]!=0).flatten()
         if candidate_subs.size>0:
             idx_sub = candidate_subs[0]+(i+1)
@@ -32,6 +59,10 @@ class GaussianElimination:
             raise ArithmeticError('System will face breakdown')
 
     def __configure_P(self)->None:
+        '''
+            Creates the class' `P_` matrix specialized in row
+            exchanges.
+        '''
         for i in range(self.A.shape[0]):
             if self.A[i,i] == 0:
                 self.__swap_P_rows(i)
@@ -39,6 +70,9 @@ class GaussianElimination:
                 continue
     
     def __configure_E(self)->None:
+        '''
+            Creates the elimination matrix `E_`. 
+        '''
         A = self.P_ @ self.A
         for i in range(1, A.shape[0]):
             for j in range(0, i):
@@ -48,6 +82,11 @@ class GaussianElimination:
         self.E_ = np.linalg.multi_dot(self.E_) if len(self.E_)>1 else np.array(self.E_[0])
         
     def __eliminate(self, b:np.ndarray=None)->Tuple[np.ndarray] | np.ndarray:
+        '''
+            Creates the eliminated coefficient matrix `U_`.
+
+            (...)
+        '''
         M = np.concatenate((self.A, b), axis=1) if b is not None else self.A
         M = np.linalg.multi_dot((self.E_, self.P_, M)) 
         return (M[:, :-1], M[:, -1]) if b is not None else M
@@ -59,7 +98,7 @@ class GaussianElimination:
             solution[i] = (b[i] - subtract)/A[i,i]
         return np.array(solution)
 
-    def eliminate(self, b:np.ndarray=None)->Tuple[np.ndarray]:
+    def eliminate(self, b:np.ndarray=None):
         self.__configure_P()
         self.__configure_E()
         self.U_ = self.__eliminate(b)
@@ -72,13 +111,9 @@ class GaussianElimination:
             else:
                 raise ValueError('You must specify a `b` vector')
         else:
-            b = b.reshape(-1,1) if b.ndim==1 else b
-            if self.U_ is None: 
-                self.eliminate(b)
-                return self.__solve(*self.U_)
+            if isinstance(self.U_, np.ndarray):
+                b = np.linalg.multi_dot((self.E_, self.P_, b))
+                return self.__solve(self.U_, b) 
             else:
-                if isinstance(self.U_, np.ndarray):
-                    return self.__solve(self.U_, b) 
-                else:
-                    warn('Elimination was already carried out considedring `b`. You did not have to inform it again on `solve`', UserWarning)
-                    return self.__solve(*self.U_)
+                warn('Elimination was already carried out considedring `b`. You did not have to inform it again on `solve`', UserWarning)
+                return self.__solve(*self.U_)
